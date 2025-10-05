b7f1b4441fc1471820bc0d8a9af9ec32595aa808
Thiago Haeitmann <135155756+ThiagoHaeitmann@users.noreply.github.com>
2025-10-04 22:37:53 -0300

    Update gunicorn.conf.py

--- nftables-frontend/gunicorn.conf.py
+++ nftables-frontend/gunicorn.conf.py
@@ -1,21 +1,20 @@
-# nftables-frontend/gunicorn.conf.py
-import os, sys, subprocess
 
-HUG_CWD = os.path.abspath("../nftables-parser")
-HUG_CMD = ["python3", "-m", "hug", "-f", "main.py"]
+import subprocess
+import sys
+import os
+import multiprocessing
+
 res = None
+workers = multiprocessing.cpu_count() * 2 + 1
+wsgi_app = "app:app"
+bind = "0.0.0.0:10001"
+
 
 def on_starting(server):
     global res
-    try:
-        print("[gunicorn] iniciando HUG com python3 -m hug em", HUG_CWD, file=sys.stderr)
-        res = subprocess.Popen(HUG_CMD, cwd=HUG_CWD, stdout=sys.stdout, stderr=sys.stderr)
-    except Exception as e:
-        print(f"[gunicorn] falha ao iniciar HUG: {e}", file=sys.stderr)
+    res = subprocess.Popen(["/usr/bin/hug", "-f", "main.py"], cwd=os.path.abspath("../nftables-parser"), shell=False,
+                           stdout=sys.stdout, stderr=sys.stderr)
+
 
 def on_exit(server):
-    try:
-        if res and res.poll() is None:
-            res.terminate()
-    except Exception:
-        pass
+    res.terminate()
